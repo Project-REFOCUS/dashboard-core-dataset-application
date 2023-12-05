1a2a3239a1aecf99ae8b4e20a14d0465974df0f9
Josue Resilien <joshrez20@gmail.com>
2023-12-05 14:29:56 -0500

    Added database yaml for osha e2e testing

---
+++ .github/workflows/e2e/createdb.sh
@@ -0,0 +1,16 @@
+mysql -uroot -e "CREATE DATABASE changesets"
+mysql -uroot "changesets" -e "
+  CREATE TABLE project_refocus (
+    id INTEGER UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    file_path VARCHAR(125) NOT NULL,
+    applied TINYINT(1) NULL DEFAULT '1',
+    environment VARCHAR(50) NOT NULL,
+    reference_database VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
+  )
+"
+mysql -uroot -e "CREATE DATABASE project_refocus"
+mysql_config_editor set --login-path=root --user root
+
+# Display information about the mysql server
+mysql --login-path=root -e "\s"
